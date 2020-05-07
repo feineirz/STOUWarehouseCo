ea75f709b1b66d37190512b987711082b0b3f4ec
feinz <feineirz@live.com>
2020-05-07 20:38:08 +0700

    .

--- src/APP/Controllers/View.java
+++ src/APP/Controllers/View.java
@@ -2,6 +2,18 @@ package APP.Controllers;
 
 import javax.swing.JFrame;
 
+import APP.Designers.DefaultDesigner;
+
 public class View {
+	
+	// Preload Assets
+	static 
+	JFrame defaultDesiner = new DefaultDesigner();
+	
+	public static void main(String[] arg) {
+		
+		defaultDesiner.setVisible(true);
+		
+	}
 	
 }
